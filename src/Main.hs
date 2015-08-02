2c40c0306c009af95495449cd325da7a688ec2c0
Sanjoy Das <sanjoy@playingwithpointers.com>
2015-08-02 12:40:24 -0700

    clean up main: no need for fancy cmd args parsing.

--- src/Main.hs
+++ src/Main.hs
@@ -2,35 +2,11 @@
 
 module Main where
 
-import System.Console.CmdArgs.Implicit
-import System.Exit
-
 import MissionariesAndCannibals
 import SlidingPuzzle
 import UCS
 
-data ProblemKind = NonePK | MissionariesAndCannibalsPK | SlidingPuzzlePK deriving(Show, Eq, Data)
-data SolverKind = NoneSK | UniformCostSearchSK deriving(Show, Eq, Data)
-
-data Arguments = Arguments { problem :: ProblemKind, solver :: SolverKind } deriving(Show, Eq, Data)
-
-arguments = Arguments { problem = NonePK &= help "Missionaries and Cannibals",
-                        solver = NoneSK &= help "Uniform cost search" }
-
 main :: IO ()
 main = do
-  (Arguments problemKind solverKind) <- cmdArgs arguments
-  getProblem <- case problemKind of
-    NonePK -> do
-      putStrLn "please specify a problem type using --problem"
-      exitWith $ ExitFailure 1
-      return undefined
-    MissionariesAndCannibalsPK -> return missionariesAndCannibals
-    SlidingPuzzlePK -> return slidingPuzzle
-  getSolver <- case solverKind of
-    NoneSK -> do
-      putStrLn "please specify a solver type using --solver"
-      exitWith $ ExitFailure 1
-      return undefined
-    UniformCostSearchSK -> return uniformCostSearch
-  print $ getSolver getProblem
+  print $ uniformCostSearch missionariesAndCannibals
+  print $ uniformCostSearch slidingPuzzle
