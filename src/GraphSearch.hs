fa9b06165a1a83117a088c1219772015c3ad3e9d
Sanjoy Das <sanjoy@playingwithpointers.com>
2015-08-02 14:15:15 -0700

    Add a prettyShowSolution function.
    Less debuggable but prettier than default derived Show for a Maybe (Node a)

--- src/GraphSearch.hs
+++ src/GraphSearch.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE RankNTypes #-}
-module GraphSearch(graphSearch, Node(..), FrontierStore(..)) where
+
+module GraphSearch(graphSearch, Node(..), FrontierStore(..), prettyShowSolution) where
 
 import qualified Data.Set as S
+import qualified Data.List as L
+
 import SearchProblem
 
 data Node state =
-    Node { getNodeCost :: Integer, getNodeParent :: Node state, getState :: state }
+    Node { getNodeCost :: Integer, getNodeParent :: Node state, getNodeState :: state }
   | NodeRoot deriving(Show)
 
 instance Eq a => Eq (Node a) where
@@ -16,6 +19,16 @@ instance Eq a => Ord (Node a) where
 
 data FrontierStore f v =
   FS { getEmptyStore :: f, getPop :: Ord f => f -> Maybe (f, Node v), getInsert :: Node v -> f -> f }
+
+prettyShowSolution :: (Show a) => Maybe (Node a) -> String
+prettyShowSolution Nothing = "No Solution"
+prettyShowSolution (Just n) =
+  let cost = getNodeCost n
+      states = L.unfoldr unfoldNode n
+      unfoldNode n = case n of
+        NodeRoot -> Nothing
+        _ -> Just (getNodeState n, getNodeParent n)
+  in "Cost = " ++ show cost ++ ": " ++ (L.intercalate "; " $ map show states)
 
 graphSearch :: (Ord v, Ord f, Show v, Show f) => SearchProblem v act -> FrontierStore f v -> Maybe (Node v)
 graphSearch ssp store =
@@ -30,20 +43,20 @@ graphSearch ssp store =
       initialFrontier = frontierInsert (Node 0 NodeRoot (getInitialState ssp)) frontierEmpty
 
       expandChildren node =
-        let actsAndCosts = genActs (getState node)
+        let actsAndCosts = genActs (getNodeState node)
             mapFst f (a,b) = (f a, b)
-            statesAndCosts = map (mapFst (apply $ getState node)) actsAndCosts
+            statesAndCosts = map (mapFst (apply $ getNodeState node)) actsAndCosts
         in map (\(st, cost) -> Node (cost + getNodeCost node) node st) statesAndCosts
 
       searchIter frontier explored = frontierPop frontier >>= searchIterHelper explored
 
       searchIterHelper explored (frontier, bestNode) =
-        if goalPred (getState bestNode) then
+        if goalPred (getNodeState bestNode) then
           Just bestNode
         else
           let children = expandChildren bestNode
-              explored' = S.insert (getState bestNode) explored
-              newChildren = filter (\c -> getState c `S.notMember` explored') children
+              explored' = S.insert (getNodeState bestNode) explored
+              newChildren = filter (\c -> getNodeState c `S.notMember` explored') children
               frontier' = foldl (flip frontierInsert) frontier newChildren
           in searchIter frontier' explored'
 
